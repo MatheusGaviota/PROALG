64e1756c68e296e38dd1f8fb7e5a4445cf243f99
Matheus Souza Rodrigues <79955422+MatheusGaviota@users.noreply.github.com>
2021-10-25 16:43:11 -0300

    Delete JavaScript directory

--- JavaScript/Aula1_variaveis/variaveis.js
+++
@@ -1,5 +0,0 @@
-var num = 10;
-var num2 = 200;
-var texto = "Campanha Arrecadação";
-var cliente = "Matheus Souza";
-pontos = num + num2;
--- JavaScript/Aula2_entrada_dados_funcao/entrada.js
+++
@@ -1,6 +0,0 @@
-function pedirvalores(){
-    num1 = parseInt(prompt("Digite o primeiro número"));
-    num2 = parseInt(prompt("Digite o segundo número"));
-    soma = num1+num2
-    subtracao = num1-num2
-}
